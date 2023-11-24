785a42e35740c8203d42273331a1d22df40cf383
Mathéo Lang <matsuel0308@gmail.com>
2023-11-24 22:28:02 +0100

    add table for display recap

--- kk.py
+++ kk.py
@@ -1,8 +1,12 @@
 import os
 import shutil
 import subprocess
+from rich import print
+from rich.table import Table
 
 def copyCommits():
+    commits_list = []
+    commits_message = []
 
     # Chemin du dépôt Git d'origine
     repo_path = "C:/Users/matsu/Documents/challenge-go-23"
@@ -19,28 +23,27 @@ def copyCommits():
     # Parcours de la liste des commits
     for commit in commits:
         # Checkout du commit dans le dépôt d'origine
-        # subprocess.check_call(["git", "checkout", commit], cwd=repo_path)
-        os.system(f'git checkout {commit} > nul')
+        subprocess.run(["git","checkout",commit], cwd=repo_path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Copie du contenu du dépôt d'origine vers le dossier de destination en excluant .git
         copy_without_git(repo_path, dest_path)
 
         # Ajout des fichiers copiés au dépôt de destination
-        # subprocess.run(["git", "add", "."], cwd=dest_path)
-        os.system('git add . > nul')
+        subprocess.run(["git", "add", "."], cwd=dest_path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Récupération du message du commit d'origine
         commit_info = subprocess.check_output(["git", "log", "-n", "1", "--pretty=format:%s"], cwd=repo_path).decode().strip()
 
-        subprocess.run(["git", "branch", "--unset-upstream", "origin/master"], cwd=dest_path)
+        subprocess.run(["git", "branch", "--unset-upstream", "origin/master"], cwd=dest_path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Commit des modifications dans le dépôt de destination avec le message du commit d'origine et la date du commit d'origine
-        # subprocess.run(["git", "commit", "-m", commit_info], cwd=dest_path)
-        # subprocess.run(["git", "commit", "--date", commit_date, "-m", commit_info], cwd=dest_path)
-        os.system(f'git commit -m "{commit_info}" > nul')
+        subprocess.run(["git", "commit", "-m", commit_info], cwd=dest_path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-        # subprocess.run(["git", "push", "--set-upstream", "origin", "main"], cwd=dest_path)
-        os.system('git push --set-upstream origin main > nul')
+        subprocess.run(["git", "push", "--set-upstream", "origin", "main"], cwd=dest_path, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        print(f"Commit {commit} copié")
+    
+    show_Commits(commits_list, commits_message)
 
 
 # Fonction pour copier les fichiers et dossiers en excluant .git
@@ -55,3 +58,16 @@ def copy_without_git(src, dst):
                 shutil.copy2(source_item, dest_item)  # Copy file and preserve metadata
 
 copyCommits()
+
+
+
+def show_Commits(commits_list, commits_message):
+    table = Table(title="Commits")
+    table.add_column("Hash", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Message", style="magenta")
+    table.add_column("Statut", justify="right", style="green")
+
+    for i in range(len(commits_list)):
+        table.add_row(commits_list[i], commits_message[i], "✅")
+    
+    return table
